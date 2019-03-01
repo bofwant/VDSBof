6edf3f4b132829e6afb1b3caebd9773fefbd7998
Daniel <maplerhema@gmail.com>
2019-03-01 13:58:08 -0400

    working on the app layout

--- AndroidApp/app/src/main/java/com/bofwant/esp32vds/MainActivity.java
+++ AndroidApp/app/src/main/java/com/bofwant/esp32vds/MainActivity.java
@@ -3,6 +3,8 @@ package com.bofwant.esp32vds;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
@@ -11,7 +13,12 @@ import android.widget.ToggleButton;
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
-
+    final Fragment fragment1 = new HomeFragment();
+    final Fragment fragment2 = new GraphFragment();
+    final Fragment fragment3 = new SettingsFragment();
+    final Fragment fragment4 = new ControlFragment();
+    final FragmentManager fm = getSupportFragmentManager();
+    Fragment active = fragment1;
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -21,11 +28,14 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.navigation_home:
                     mTextMessage.setText(R.string.title_home);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_graph:
+                    mTextMessage.setText(R.string.title_Graph);
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_settings:
+                    mTextMessage.setText(R.string.title_settings);
+                    return true;
+                case R.id.navigation_control:
+                    mTextMessage.setText(R.string.title_control);
                     return true;
             }
             return false;
@@ -36,10 +46,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mTextMessage = (TextView) findViewById(R.id.message);
+        //mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        ToggleButton conectButton = (ToggleButton) findViewById(R.id.conectButton);
+        //ToggleButton conectButton = (ToggleButton) findViewById(R.id.conectButton);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        fm.beginTransaction().add(R.id.main_container, fragment4, "4").hide(fragment3).commit();
+        fm.beginTransaction().add(R.id.main_container, fragment3, "3").hide(fragment3).commit();
+        fm.beginTransaction().add(R.id.main_container, fragment2, "2").hide(fragment2).commit();
+        fm.beginTransaction().add(R.id.main_container,fragment1, "1").commit();
     }
 
 }
